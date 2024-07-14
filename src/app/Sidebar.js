e82f6a5e1b3046752787d15f82fced19107f743a
Jacob <Jacobshortall@gmail.com>
2024-07-14 22:40:20 +0100

    Remove sidebar

--- src/app/Sidebar.js
+++
@@ -1,7 +0,0 @@
-const Sidebar = () => {
-    console.log('sidebar server');
-
-    return <h1>server sidebar</h1>;
-};
-
-export default Sidebar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,5 @@
 import HomeContentWrapper from './HomeContent';
-import Sidebar from './Sidebar';
 
 export default function Home() {
-    return (
-        <HomeContentWrapper>
-            <Sidebar />
-        </HomeContentWrapper>
-    );
+    return <HomeContentWrapper />;
 }
