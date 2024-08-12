36dad05d892289866ef87a33c182844ba0fa1d22
Jacob <Jacobshortall@gmail.com>
2024-08-12 21:59:41 +0100

    Optimise

--- src/app/CalendarDay.js
+++ src/app/CalendarDay.js
@@ -3,17 +3,19 @@
 import CalendarEvent from './CalendarEvent';
 
 const CalendarDay = ({ day, events }) => {
-    if (!events) return;
-
     return (
         <div className="calendar-day">
             <span className="cd-day">{day}</span>
 
-            <div className="cd-events">
-                {events.map((eventInfo, index) => (
-                    <CalendarEvent key={index} eventInfo={eventInfo} />
-                ))}
-            </div>
+            {events ? (
+                <div className="cd-events">
+                    {events.map((eventInfo, index) => (
+                        <CalendarEvent key={index} eventInfo={eventInfo} />
+                    ))}
+                </div>
+            ) : (
+                ''
+            )}
         </div>
     );
 };
--- src/app/CalendarEvent.js
+++ src/app/CalendarEvent.js
@@ -1,6 +1,10 @@
 const CalendarEvent = ({ eventInfo }) => {
+    const handleClick = () => {
+        return;
+    };
+
     return (
-        <div className="calendar-event">
+        <div className="calendar-event" onClick={handleClick}>
             <span>{eventInfo.title}</span>
             <span>{eventInfo.time.getHours()}</span>
         </div>
